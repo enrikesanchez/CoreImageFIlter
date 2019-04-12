c20d873ec3d0a0cbde16febe3726bcb78df7960c
user148189 <user148189@74-80-245-25.ic2net.net>
2019-04-12 00:08:22 -0700

    First Draft

--- CoreImageFIlter/ViewController.swift
+++ CoreImageFIlter/ViewController.swift
@@ -2,7 +2,7 @@
 //  ViewController.swift
 //  CoreImageFIlter
 //
-//  Created by user148189 on 4/11/19.
+//  Created by esanchez on 4/11/19.
 //  Copyright © 2019 Tec. All rights reserved.
 //
 
@@ -10,11 +10,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        guard let image = imageView?.image, let cgimg = image.cgImage else {
+            print("imageView doesn't have an image!")
+            return
+        }
+        
+        let coreImage = CIImage(cgImage: cgimg)
+        
+        let filter = CIFilter(name: "CIPhotoEffectNoir")
+        filter?.setValue(coreImage, forKey: kCIInputImageKey)
+        
+        if let output = filter?.value(forKey: kCIOutputImageKey) as? CIImage {
+            let filteredImage = UIImage(ciImage: output)
+            imageView?.image = filteredImage
+        }
+            
+        else {
+            print("image filtering failed")
+        }
     }
-
-
 }
 
